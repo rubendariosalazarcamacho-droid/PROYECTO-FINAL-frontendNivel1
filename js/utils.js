41742d4e789b24e4dc1348fd50c366b2f877027d
Ruben Salazar <rubendariosalazarcamacho@gmail.com>
2026-02-28 23:58:31 -0500

    se crean funciones para: insertar estancia y para insertar todas las estancias

--- js/utils.js
+++ js/utils.js
@@ -3,3 +3,37 @@
  * Este archivo contiene funciones auxiliares que serán utilizadas y llamadas
  * desde el archivo principal para realizar varias operaciones.
  */
+import { stays } from "./stays";
+
+const contenedorEstancias=document.querySelector ("#contenedorEstancias")
+
+function insertarEstancia (elementoActual){
+    let estancia =elementoActual
+
+    const {type, beds, rating, title} = estancia;
+
+    contenedorEstancias.innerHTML += `
+
+    <div class="flex flex-col gap-2" >  
+    <div><img class="rounded-3xl w-[345px] h-[235px]" src="${estancia.photo}" alt="foto estancia"></div>
+        <div class="flex justify-between px-2">
+            <div class="text-xs">${type} . ${beds} beds</div>
+            <div class="flex text-xs gap-2"><img class="w-[16px] h-[16px]" src="./public/imagenes/star.svg" alt="estrella"> ${rating}</div>
+        </div>
+    <p class="text-[13px] font-medium">${title}</p> 
+    </div>  
+    
+    `  
+}
+
+
+
+function insertarTodasLasEstancias(){
+    contenedorEstancias.innerHTML= ""
+
+    stays.forEach((elementoActual) =>{
+        insertarEstancia(elementoActual)
+
+    })
+}
+insertarTodasLasEstancias()
